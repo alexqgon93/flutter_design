b5d7deb06e4c21c9d8de13d45f22cb55aacbc00e
Alexandre Esquerdo <alex.esquerdo@the-cocktail.com>
2023-03-02 13:50:20 +0100

    feat: added new optimization on card component.

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -62,6 +62,32 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var column = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 30,
+          child: Icon(icon, size: 30, color: Colors.white),
+        ),
+        const SizedBox(height: 30),
+        Text(
+          text,
+          style: TextStyle(color: color, fontSize: 18),
+        ),
+      ],
+    );
+    return _CardBackground(child: column);
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
       child: ClipRRect(
@@ -73,21 +99,7 @@ class _SingleCard extends StatelessWidget {
             decoration: BoxDecoration(
                 color: const Color.fromRGBO(62, 66, 107, 0.7),
                 borderRadius: BorderRadius.circular(20)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: color,
-                  radius: 30,
-                  child: Icon(icon, size: 30, color: Colors.white),
-                ),
-                const SizedBox(height: 30),
-                Text(
-                  text,
-                  style: TextStyle(color: color, fontSize: 18),
-                ),
-              ],
-            ),
+            child: child,
           ),
         ),
       ),
